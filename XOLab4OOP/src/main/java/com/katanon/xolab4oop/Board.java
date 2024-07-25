e22de83c09e712b0d230c9484f7ddb94ad27ac5c
KTN-katanon <66160196@go.buu.ac.th>
2024-07-25 18:16:19 +0700

    Update Board.java
    Add getter  setter  and toString

--- XOLab4OOP/src/main/java/com/katanon/xolab4oop/Board.java
+++ XOLab4OOP/src/main/java/com/katanon/xolab4oop/Board.java
@@ -19,6 +19,35 @@ public class Board {
         this.p2 = p2;
     }
 
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public Player getP1() {
+        return p1;
+    }
+
+    public void setP1(Player p1) {
+        this.p1 = p1;
+    }
+
+    public Player getP2() {
+        return p2;
+    }
+
+    public void setP2(Player p2) {
+        this.p2 = p2;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" + "board=" + board + ", p1=" + p1 + ", p2=" + p2 + '}';
+    }
+    
     public void printBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {

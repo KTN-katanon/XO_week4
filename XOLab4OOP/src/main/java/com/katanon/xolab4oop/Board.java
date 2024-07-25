32e2b664344831236385e7f8efc3c1269f21304e
KTN-katanon <66160196@go.buu.ac.th>
2024-07-25 17:37:22 +0700

    Update Board.java
    Add printBoard()

--- XOLab4OOP/src/main/java/com/katanon/xolab4oop/Board.java
+++ XOLab4OOP/src/main/java/com/katanon/xolab4oop/Board.java
@@ -26,4 +26,13 @@ public class Board {
         }
     }
     
+    public void printBoard(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
 }

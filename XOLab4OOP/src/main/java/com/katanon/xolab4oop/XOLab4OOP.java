3378b4a8a682aa31bbdbed615bb3ca515f776e0f
KTN-katanon <66160196@go.buu.ac.th>
2024-07-25 16:55:24 +0700

    Create project

---
+++ XOLab4OOP/src/main/java/com/katanon/xolab4oop/XOLab4OOP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.xolab4oop;
+
+/**
+ *
+ * @author nonku
+ */
+public class XOLab4OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("asdasdasd");
+    }
+}

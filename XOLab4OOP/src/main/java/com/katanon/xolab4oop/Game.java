f7def30e722b9615f9b7b60b1f8a9d80c0cf9ec6
KTN-katanon <66160196@go.buu.ac.th>
2024-07-26 16:31:36 +0700

    Update Game.java
    Add showBoard() and Game constructor

--- XOLab4OOP/src/main/java/com/katanon/xolab4oop/Game.java
+++ XOLab4OOP/src/main/java/com/katanon/xolab4oop/Game.java
@@ -9,11 +9,34 @@ package com.katanon.xolab4oop;
  * @author nonku
  */
 public class Game {
-    public void play(){
-        this.showWelcome();
+    private Player o;
+    private Player x;
+    private Board board;
+
+    public Game() {
+        o = new Player('O');
+        x = new Player('X');
     }
     
-    public void showWelcome(){
+    
+    
+    public void play(){
+        this.showWelcome();
+        board = new Board(o,x);
+        this.showBoard();
+    }
+    
+    private void showWelcome(){
         System.out.println("Welcome to XO Game");
+    }
+    
+    private void showBoard(){
+        char [][] table = board.getBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

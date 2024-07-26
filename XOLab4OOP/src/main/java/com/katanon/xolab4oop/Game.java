bd3f13967b6c8d740b392d62bb7e75b3de58dfee
KTN-katanon <66160196@go.buu.ac.th>
2024-07-26 17:13:05 +0700

    Update Game.java
    Add input error case in inputRowCol

--- XOLab4OOP/src/main/java/com/katanon/xolab4oop/Game.java
+++ XOLab4OOP/src/main/java/com/katanon/xolab4oop/Game.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.katanon.xolab4oop;
+
 import java.util.*;
 
 /**
@@ -10,9 +11,10 @@ import java.util.*;
  * @author nonku
  */
 public class Game {
+
     private Player o;
     private Player x;
-    private int row,col;
+    private int row, col;
     private Board board;
     Scanner sc = new Scanner(System.in);
 
@@ -20,30 +22,39 @@ public class Game {
         o = new Player('O');
         x = new Player('X');
     }
-    
-    
-    
-    public void play(){
+
+    public void play() {
         this.showWelcome();
-        board = new Board(o,x);
+        board = new Board(o, x);
         board.printBoard();
         inputRowCol();
         board.printBoard();
     }
-    
-    private void showWelcome(){
+
+    private void showWelcome() {
         System.out.println("Welcome to XO Game");
     }
-    
-    public void inputRowCol(){
-        while(true){
-            System.out.print("Please input row, col: ");
-            row = sc.nextInt() - 1;
-            col = sc.nextInt() - 1;
-            if(board.setRowCol(row, col)){
-                return;
+
+    public void inputRowCol() {
+        while (true) {
+            try {
+                System.out.print("Please input row, col: ");
+                row = sc.nextInt() - 1;
+                col = sc.nextInt() - 1;
+
+                if (board.setRowCol(row, col)) {
+                    return;
+                } else {
+                    System.out.println("Player is already in that spot!");
+                }
             }
-            System.out.println("Player is already in that spot!");
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("This position is outside of the board!");
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid input! Please input integers!");
+                sc.next();
+            }
         }
     }
 }
